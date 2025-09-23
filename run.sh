67592e34c5ba2afd873fad0565488519930c6862
PengyuJiang <1593100965@qq.com>
2025-09-23 15:48:08 +0800

    run sh file

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 python imitate_episodes.py \
-    --eval \
     --task_name sim_pick_n_place_cube_scripted \
     --ckpt_dir ./ckpt_dir/pick2 \
     --policy_class ACT \
@@ -9,10 +8,11 @@ python imitate_episodes.py \
     --nheads 16 \
     --batch_size 6 \
     --dim_feedforward 4096 \
-    --num_epochs 500 \
+    --num_epochs 2000 \
     --lr 1e-5 \
     --seed 0 \
     --backbone resnet50 \
     --l1_weight 1.0 \
     --recon_loss huber --huber_beta 0.1 \
-    --adam_beta1 0.9 --adam_beta2 0.98
+    --adam_beta1 0.9 --adam_beta2 0.98 \
+    # --eval \
